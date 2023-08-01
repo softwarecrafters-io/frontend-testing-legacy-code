b4025ffe68886bc39177f1ee810ce536a7792b72
Miguel A. Gómez <>
2023-08-01 19:23:28 +0100

    end to end tests wip

--- src/tests/e2e/todoApp.test.ts
+++ src/tests/e2e/todoApp.test.ts
@@ -9,6 +9,34 @@ describe('TodoList App', () => {
     cy.contains('h1', 'TODOLIST APP');
   });
 
+  it('should be able to add a new todo', () => {
+    cy.get('.todo-input').type(todoText);
+    cy.get('.add-todo-button').click();
 
+    cy.get('.todo-delete-button').click();
+
+    cy.contains('.todo-list-item', todoText).should('not.exist');
+  });
+
+  it('should be able to delete a todo', () => {
+    cy.get('.todo-input').type(todoText);
+    cy.get('.add-todo-button').click();
+    cy.get('.todo-list-item').should('contain', todoText);
+
+    cy.get('.todo-delete-button').first().click();
+    cy.contains('.todo-list-item', todoText).should('not.exist');
+  });
+
+  it('should be able to mark a todo as complete', () => {
+    cy.get('.todo-input').type(todoText);
+    cy.get('.add-todo-button').click();
+
+    cy.get('.todo-mark-button').click();
+
+    cy.get('.todo-mark-button').should('contain', 'Mark as Incomplete');
+
+    cy.get('.todo-delete-button').click();
+    cy.contains('.todo-list-item', todoText).should('not.exist');
+  });
 });
 
